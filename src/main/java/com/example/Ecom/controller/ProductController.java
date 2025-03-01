7e420ccb3128f1706953c3f4a7b36521dbbf49db
lalukrish <lalukrishnavallil01@gmail.com>
2025-03-01 15:40:28 +0530

    fix:Cross origin added

--- src/main/java/com/example/Ecom/controller/ProductController.java
+++ src/main/java/com/example/Ecom/controller/ProductController.java
@@ -4,13 +4,15 @@ package com.example.Ecom.controller;
 import com.example.Ecom.model.Product;
 import com.example.Ecom.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+    @RestController
+    @CrossOrigin
     @RequestMapping("/api")
     public class ProductController {
 
